81ef6a16a178a8b26103d14549f151caea5cc4dc
fengyuecanzhu <1021300691@qq.com>
2022-11-17 18:01:09 +0800

    Add copyright

--- src/main/resources/des.js
+++ src/main/resources/des.js
@@ -1,9 +1,20 @@
-/** 
-* DESº”√‹Ω‚√‹ 
-* @Copyright Copyright (c) 2006 
-* @author Guapo 
-* @see DESCore 
-*/  
+/*
+ * This file is part of Sdu-Seat
+ * Sdu-Seat is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Sdu-Seat is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Sdu-Seat.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Copyright (C)  2022 fengyuecanzhu
+ */
   
 /* 
 * encrypt the string to string made up of hex 
